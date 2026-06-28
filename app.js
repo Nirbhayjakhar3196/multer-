21ac7a39cdd6739bad43eb0bee57524309cffca5
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-06-28 17:21:59 +0530

    feat:Added error middleware

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 const uploadRoutes = require("./src/routes/uploadRoutes");
+const errorMiddleware = require("./src/middleware/errorMiddleware")
 
 app.use("/api", uploadRoutes);
+
+app.use(errorMiddleware)
 
 module.exports = app;
